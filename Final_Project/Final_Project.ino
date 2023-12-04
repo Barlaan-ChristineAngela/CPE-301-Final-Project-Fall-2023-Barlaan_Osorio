32d688bca6d80a065f82da521b23c8612baaa9df
Barlaan-ChristineAngela-1102 <christineangelab@nevada.unr.edu>
2023-12-04 03:03:16 -0800

    draft

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,6 +1,9 @@
-#include <dht.h>
+#include <dht.h> //temp and humidity sensor
+#include <LiquidCrystal.h> //LCD
 
 dht DHT; //temp and humidity sensor
+const int RS = 11, EN = 12, D4 = 2, D5 = 3, D6 = 4, D7 = 5; //LCD pins
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7); //LCD
 
 #define DHT11_PIN 2 //temp and humidity sensor
 #define POWER_PIN 3 //water sensor
@@ -17,18 +20,30 @@ void loop() {
 
 }
 
-//change to print on LCD display
 void TempHumiditySensor(){
-  int chk = DHT.read11(DHT11_PIN);  //begin temp/humidity sensor
+  int chk = DHT.read11(DHT11_PIN);
+
+  /*for testing; do not include in final program
   Serial.print("Temperature = ");
   Serial.println(DHT.temperature);
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
-  delay(1000);                      //end temp/humidity sensor
+  delay(1000);
+  */
+
+  //print to LCD display
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temperature = ");
+  lcd.print(DHT.temperature);
+  lcd.setCursor(1,0);
+  lcd.print("Humidity = ");
+  lcd.print(DHT.humidity);
+  delay(60000); //update every 1 minute (60 seconds)
 }
 
 //must change code to use ADC method
-void WaterSensor(){
+  void WaterSensor(){
   int value = 0;
 
   //from example slides
@@ -47,9 +62,10 @@ void FanMotor(){
 
 }
 
-void LCDDisplay(){
+/*void LCDDisplay(){
 
 }
+*/
 
 void RTCModule(){
 
